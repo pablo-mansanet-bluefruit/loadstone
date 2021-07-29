03702e9773bdaf66cfedcc74040179a80e50c0a3
pablo-mansanet-bluefruit <pablo.mansanet@bluefruit.co.uk>
2021-07-29 12:46:22 +0200

    Define bootloader manual port

--- src/ports/stm32f446_manual/bootloader.rs
+++ src/ports/stm32f446_manual/bootloader.rs
@@ -2,36 +2,71 @@
 //!
 //! This is a manual port, meant to showcase a build that does not result
 //! from configuration in loadstone_front.
-use crate::devices::update_signal::ReadUpdateSignal;
-use crate::devices;
+
+use crate::devices::image;
 use crate::{devices::bootloader::Bootloader, error};
 use crate::error::Error;
-use blue_hal::hal::null::{NullError, NullFlash, NullSerial};
+use blue_hal::drivers::stm32f4;
+use blue_hal::drivers::stm32f4::rcc::Clocks;
+use blue_hal::drivers::stm32f4::serial::UsartExt;
+use blue_hal::hal::null::{NullError, NullFlash};
+use blue_hal::hal::time::Bps;
+use blue_hal::stm32pac::USART1;
+use super::pin_configuration::GpioExt;
+use super::pin_configuration::*;
 
 use blue_hal::drivers::stm32f4::{flash, serial, systick::SysTick};
+use blue_hal::{KB, stm32pac};
 
 #[cfg(feature="ecdsa-verify")]
 use crate::devices::image::EcdsaImageReader as ImageReader;
 #[cfg(not(feature="ecdsa-verify"))]
 use crate::devices::image::CrcImageReader as ImageReader;
 
-type ExternalFlash = NullFlash;
-type Serial = NullSerial;
-pub struct UpdateSignal;
+use super::update_signal::UpdateSignal;
+type SerialPins = (Pb6<AF7>, Pb7<AF7>);
+type Serial = stm32f4::serial::Serial<USART1, SerialPins>;
 
-impl ReadUpdateSignal for UpdateSignal {
-    fn read_update_plan(&self) -> devices::update_signal::UpdatePlan {
-        todo!()
-    }
-}
+type ExternalFlash = NullFlash;
 
 impl Default for Bootloader<ExternalFlash, flash::McuFlash, Serial, SysTick, ImageReader, UpdateSignal> {
     fn default() -> Self { Self::new() }
 }
 
+const NUMBER_OF_MCU_BANKS: usize = 1usize;
+pub static MCU_BANKS: [image::Bank<flash::Address>; NUMBER_OF_MCU_BANKS] = [image::Bank {
+    index: 1u8,
+    bootable: true,
+    location: flash::Address(0x0801_0000),
+    size: KB!(448),
+    is_golden: false,
+}];
+
 impl Bootloader<ExternalFlash, flash::McuFlash, Serial, SysTick, ImageReader, UpdateSignal> {
     pub fn new() -> Self {
-        todo!();
+        let mut peripherals = stm32pac::Peripherals::take().unwrap();
+        let mcu_flash = flash::McuFlash::new(peripherals.FLASH).unwrap();
+
+        let gpiob = peripherals.GPIOB.split(&mut peripherals.RCC);
+        let clocks = Clocks::hardcoded(peripherals.RCC);
+        let pins = (gpiob.pb6, gpiob.pb7);
+
+        let config = serial::config::Config::default().baudrate(Bps(115_200));
+        let serial = peripherals.USART1.constrain(pins, config, clocks).unwrap();
+
+        Self {
+            mcu_flash,
+            mcu_banks: &MCU_BANKS,
+            external_flash: None,
+            external_banks: &[],
+            serial: Some(serial),
+            boot_metrics: Default::default(),
+            start_time: None,
+            recovery_enabled: true,
+            update_signal: None,
+            greeting: "Loadstone f446 Manual Port",
+            _marker: Default::default(),
+        }
     }
 }
 
