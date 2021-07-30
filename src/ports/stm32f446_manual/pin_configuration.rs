cdcb8b4aa6543e27698e747035c0d0b1028a72b8
pablo-mansanet-bluefruit <pablo.mansanet@bluefruit.co.uk>
2021-07-30 10:24:17 +0200

    Clean up pin config

--- src/ports/stm32f446_manual/pin_configuration.rs
+++ src/ports/stm32f446_manual/pin_configuration.rs
@@ -6,13 +6,9 @@ use blue_hal::{
     drivers::stm32f4::serial::*,
     enable_gpio, enable_qspi, enable_serial, enable_spi, gpio, gpio_inner, pin_rows,
 };
-pub type UsartPins = ();
-pub type Serial = blue_hal::hal::null::NullSerial;
-pub type ExternalFlash = blue_hal::hal::null::NullFlash;
-pub type QspiPins = ();
 
 enable_gpio!();
-gpio ! (a , [(0 , Input < Floating >) , (1 , Input < Floating >) ,]) ;
+gpio ! (a, []);
 gpio!(b, [(6, AF7 as TxPin<USART1>), (7, AF7 as RxPin<USART1>),]);
 gpio!(c, []);
 gpio!(d, []);
@@ -20,25 +16,3 @@ gpio!(e, []);
 gpio!(f, []);
 gpio!(g, []);
 gpio!(h, []);
-#[allow(unused)]
-pub fn pins(
-    gpioa: stm32pac::GPIOA,
-    gpiob: stm32pac::GPIOB,
-    gpioc: stm32pac::GPIOC,
-    gpiod: stm32pac::GPIOD,
-    gpioe: stm32pac::GPIOE,
-    gpiof: stm32pac::GPIOF,
-    gpiog: stm32pac::GPIOG,
-    gpioh: stm32pac::GPIOH,
-    rcc: &mut stm32pac::RCC,
-) -> (UsartPins, QspiPins) {
-    let gpioa = gpioa.split(rcc);
-    let gpiob = gpiob.split(rcc);
-    let gpioc = gpioc.split(rcc);
-    let gpiod = gpiod.split(rcc);
-    let gpioe = gpioe.split(rcc);
-    let gpiof = gpiof.split(rcc);
-    let gpiog = gpiog.split(rcc);
-    let gpioh = gpioh.split(rcc);
-    ((), ())
-}
